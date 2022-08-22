7f576044b90a1b7050ad992c98740695de510d37
SpLiYvIiI <32437231+SpLiYvIiI@users.noreply.github.com>
2022-08-22 20:54:17 +0400

    some message

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class FileReader {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    public static Profile getDataFromFile(File file) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (FileInputStream stream = new FileInputStream(file)) {
+            int ch;
+            while ((ch = stream.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        List<String> res = Arrays.asList(stringBuilder.toString().split("\n"));
+
+        String name = res.get(0).split(": ")[1];
+        int age = Integer.parseInt(res.get(1).split(": ")[1]);
+        String email = res.get(2).split(": ")[1];
+        Long phone = Long.parseLong(res.get(3).split(": ")[1]);
+
+        return new Profile(name, age, email, phone);
     }
+
 }
